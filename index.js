4089269c6edeb8d94922bc95c2643b1f911eaf04
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2019-04-02 20:28:04 -0400

    create jeopardy board and initial js

---
+++ index.js
@@ -0,0 +1,11 @@
+const box = document.querySelector('box');
+
+function changeText(box){
+    box.innerHTML = "Question!";
+    // setInterval(function(){prompt('What\'s your answer?'), 2000})
+    prompt('What is lexical scope?')
+
+    if(prompt = this){
+        alert('You did it!')
+    }
+}
